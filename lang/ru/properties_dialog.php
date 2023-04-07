f90001a6780a2bb4af3bc8dee88b87dc81864c2d
GoetzDeBouville <count.of.tuscan@gmail.com>
2023-04-07 22:50:34 +0300

    lang lib

---
+++ lang/ru/properties_dialog.php
@@ -0,0 +1,4 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die;
+
+$MESS['LINK_STRING'] = 'Ссылка на файл';
---
+++ lang/ru/readdocxactivity.php
@@ -0,0 +1,5 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die;
+
+$MESS['ERROR_NO_LINK'] = 'Не указана ссылка на файл';
+
