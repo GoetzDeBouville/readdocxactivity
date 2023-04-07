64b5eb06d0a5dfd229739655c9019256d365110d
GoetzDeBouville <count.of.tuscan@gmail.com>
2023-04-07 23:04:08 +0300

    added en lang lib

---
+++ lang/en/.description.php
@@ -0,0 +1,5 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die;
+
+$MESS['DOCX_READER_NAME'] = '.docx reader';
+$MESS['DOCX_READER_DESCRIPTION'] = 'Getting text from .docx files';
---
+++ lang/en/readdocxactivity.php
@@ -0,0 +1,5 @@
+<?php
+defined('B_PROLOG_INCLUDED') || die;
+
+$MESS['ERROR_NO_LINK'] = 'No file link';
+
